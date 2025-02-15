63cddeabaa9bdf34958d639a3320e8b0edeafd29
msDospako <fjochoa.ingenieria@gmail.com>
2025-02-15 14:32:21 -0600

    arreglos

--- LDSW/Fundamentos de programacion/Unidad 2/arreglos.js
+++ LDSW/Fundamentos de programacion/Unidad 2/arreglos.js
@@ -1,18 +1,23 @@
-function dimensiones(a) {                           // Se crea una funcion que recibe un parametro a (lado del cubo)
-    var volumen = 0;                                // Se inicializan las variables volumen y area
-    var area = 0;
-    area =  a * a;                                  // Se calcula el area del cubo
-    volumen = a * a * a;                            // Se calcula el volumen del cubo
+var miArreglo = [101,102,103,104,105,106];
+var numero= 5;           
+console.log("El arreglo inicial es: ", miArreglo);             // Se imprime el valor del arreglo inicial
+console.log("El numero a sumar a cada elemento es: ", numero); // Se imprime el valor del numero a sumar a cada elemento
 
-    return {area: area,                             // Se retorna un objeto con los valores del area y volumen
-            volumen: volumen
+arreglo(miArreglo,numero);                                     // Se llama a la funcion y se le pasan los parametros
+
+function arreglo(a,b) {                                        // Se define la funcion que recibe un arreglo y un numero
+    for (let i = 0; i < a.length; i++) {                       // Se recorre el arreglo                          
+        console.log("El elemento numero ",i,"del arreglo es ",a[i]);      
+        a[i] = a[i] + b;
+        console.log("     su valor posterior a la suma de ",b , "es ",a[i])                  // Se imprime cada elemento del arreglo
+    }
+
+    return {a: a
         }   
     
 }
-const result = dimensiones(10);                     // 10 es el valor del lado del cubo (parametro que puede cambiar para obtener diferentes resultados)
-let area = result.area;                             // Se obtiene el valor del area
-let volumen = result.volumen;                       // Se obtiene el valor del volumen
-console.log("El area y volumen de un cubo son:");   // Se imprime el resultado
-console.log("area =",area);
-console.log("volumen =",volumen);
+
+
+
+
 
