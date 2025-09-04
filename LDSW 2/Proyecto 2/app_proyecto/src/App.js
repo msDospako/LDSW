c6989cd1b22ff3e94d5a0f59dbed17710290abea
Pako <fjochoa.ingenieria@gmail.com>
2025-09-03 23:01:27 -0600

    save

--- LDSW 2/Proyecto 2/app_proyecto/src/App.js
+++ LDSW 2/Proyecto 2/app_proyecto/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Component from './Component';
+import Component from './component';
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Acceso a plataforma virtual 22 Hermanos
+          Acceso a plataforma virtual 22 hermanos
         </p>
 
         <Component />
