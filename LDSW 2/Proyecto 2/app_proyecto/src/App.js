153cc2c4773ef6444f35bc2e732e693531dfb7dc
Pako <fjochoa.ingenieria@gmail.com>
2025-09-03 22:08:19 -0600

    app

---
+++ LDSW 2/Proyecto 2/app_proyecto/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Component from './Component';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Acceso a plataforma virtual 22 Hermanos
+        </p>
+
+        <Component />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ LDSW 2/Proyecto 2/app_proyecto/src/Component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Component() {
+    return (
+        <div>
+            <input type="text" placeholder="Usuario:" /><br />
+            <input type="password" placeholder="Contraseña:" /><br />  
+            <button style={{ marginRight: '30px' }}>Acceder</button>
+            <button>Registrar</button>
+
+        </div>
+    )
+}
